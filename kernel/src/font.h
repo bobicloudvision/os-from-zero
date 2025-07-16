4c251a3744698a354e6dcc13c2f6a9dd3453f4d1
Bozhidar <bobicloudvision@gmail.com>
2025-07-16 15:47:26 +0300

    refactoring font

---
+++ kernel/src/font.h
@@ -0,0 +1,13 @@
+#ifndef FONT_H
+#define FONT_H
+
+#include <stdint.h>
+
+// Font dimensions
+#define FONT_WIDTH 8
+#define FONT_HEIGHT 8
+
+// External declaration of the 8x8 bitmap font
+extern const uint8_t font_8x8[][8];
+
+#endif // FONT_H 
